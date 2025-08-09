fc7aca11e528b3a1d808a3cb25d497cba13653ec
Dominik <dominik.jurkowski@noaignite.com>
2025-08-09 17:10:18 +0200

    feat: liquid aurora flow

---
+++ src/app/aurora-liquid/page.tsx
@@ -0,0 +1,9 @@
+import AuroraLiquid from '@/components/AuroraLiquid';
+
+export default function AuroraLiquidPage() {
+  return (
+    <div className='w-full h-screen'>
+      <AuroraLiquid />
+    </div>
+  );
+}
